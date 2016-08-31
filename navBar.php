035608b84178ea12ed0ed7166beb6233f0730d1b
vfernandez <vfernandez@Victor.axioma>
2016-08-31 09:37:35 -0300

    version 1.3 Edicion del template

--- navBar.php
+++ navBar.php
@@ -1,6 +1,5 @@
-
 <nav id="navBar" class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navContainer">
+    <div class="navContainer" >
         <div class="navbar-header">
             <button id="btnNavbar" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">navegar</span>
